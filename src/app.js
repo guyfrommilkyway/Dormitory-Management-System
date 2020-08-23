71f281577b90f6eed7a454a0d60607b4e28bc93f
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-08-24 02:55:09 +0800

    Configured path to mdbootstrap

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+async function startServer() {
+    // Initialize express application
+    const app = express()
+
+    // Assign port from config
+    const port = process.env.PORT || 3000
+
+    // Setup handlebars and engine views
+    app.set('view engine', 'hbs')
+    app.set('views', path.join(__dirname, '../templates/views'))
+    hbs.registerPartials(path.join(__dirname, '../templates/partials'))
+
+    // Setup static directory to serve
+    app.use(express.static(path.join(__dirname, '../public')))
+    app.use('/mdbootstrap', express.static(path.join(__dirname, '../node_modules/mdbootstrap')))
+
+    await require('../src/api/routes/landing')(app)
+
+    app.listen(port, () => {
+        console.log(`The server is up on ${port}.`)
+    })
+}
+
+startServer()
