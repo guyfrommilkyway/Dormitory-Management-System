6cb79928bba353da1b109fd399b1cf337673229e
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-08-24 04:11:07 +0800

    Added signup route

--- src/api/routes/landing.js
+++ src/api/routes/landing.js
@@ -1,7 +1,17 @@
 module.exports = (app) => {
+
+    // index page
     app.get('', (req, res) => {
         res.render('index', {
             title: 'DMS',
+            author: 'Almer F. Tampus'
+        })
+    })
+
+    // signup page
+    app.get('/signup', (req, res) => {
+        res.render('signup', {
+            title: 'Signup',
             author: 'Almer F. Tampus'
         })
     })
