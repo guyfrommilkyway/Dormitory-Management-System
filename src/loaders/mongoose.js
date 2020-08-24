528379403345299cba7d83cb50033fdf03c694c5
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-08-24 08:52:33 +0800

    Updated to adjust for deprecation

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 
 mongoose.connection.once('open', function () {
