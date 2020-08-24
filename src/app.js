40a7f0ae62fc7eeb624cde625fb3b12e9b2f5a23
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-08-24 08:58:36 +0800

    Restructured for code efficiency

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
-const path = require('path')
 const express = require('express')
-const hbs = require('hbs')
 
 async function startServer() {
     // Initialize express application
@@ -9,20 +7,11 @@ async function startServer() {
     // Assign port from config
     const port = process.env.PORT || 3000
 
-    // Database connection
-    await require('./loaders/mongoose')
+    // Express
+    await require('../src/loaders/express')(app, express)
 
-    // Setup handlebars and engine views
-    app.set('view engine', 'hbs')
-    app.set('views', path.join(__dirname, '../templates/views'))
-    hbs.registerPartials(path.join(__dirname, '../templates/partials'))
-
-    // Setup static directory to serve
-    app.use(express.static(path.join(__dirname, '../public')))
-    app.use('/mdbootstrap', express.static(path.join(__dirname, '../node_modules/mdbootstrap')))
-
-    // Landing routes
-    await require('../src/api/routes/landing')(app)
+    // Routes
+    await require('../src/api/index')(app)
 
     app.listen(port, () => {
         console.log(`The server is up on port ${port}.`)
---
+++ src/loaders/express.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const hbs = require('hbs')
+
+module.exports = async (app, express) => {
+    // Database connection
+    await require('./mongoose')
+
+    // Setup handlebars and engine views
+    app.set('view engine', 'hbs')
+    app.set('views', path.join(__dirname, '../../templates/views'))
+    hbs.registerPartials(path.join(__dirname, '../../templates/partials'))
+
+    // Setup static directory to serve
+    app.use(express.static(path.join(__dirname, '../../public')))
+    app.use('/mdbootstrap', express.static(path.join(__dirname, '../../node_modules/mdbootstrap')))
+}
