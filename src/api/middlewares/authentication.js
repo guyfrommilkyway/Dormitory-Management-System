9959860227690b985ab96fe4aec7c929a27a28b9
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-08-26 03:49:34 +0800

    Configured authentication middleware

--- src/api/middlewares/authentication.js
+++ src/api/middlewares/authentication.js
@@ -1,21 +1,16 @@
 const jwt = require('jsonwebtoken')
-const Cookies = require('js-cookie')
+require('cookie-parser')
 const User = require('../../models/user')
 
 const authentication = async (req, res, next) => {
     try {
-        // const token = req.header('Authorization').replace('Bearer ', '')
-        // const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        // const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        const token = req.cookies.accessToken
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
-        // if (!user) {
-        //     console.log('User doesn\'t exist')
-        // }
-
-        // req.user = user
-        // req.token = token
-
-        console.log('Authentication middleware')
+        if (!user) {
+            throw new Error()
+        }
 
         next()
     } catch (e) {
