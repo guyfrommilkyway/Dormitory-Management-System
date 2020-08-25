e9aa2568739dbad05ac58049b7e49487e5fa834f
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-08-26 03:48:56 +0800

    Restructured routes

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,4 @@
-const bodyParser = require('body-parser')
-const userRouter = require('./routes/user')
-
-module.exports = async (app, express) => {
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true }))
-    await require('./routes/landing')(app)
-    app.use(userRouter)
+module.exports = (app) => {
+    await require('../api/routes/landing')(app)
+    await require('../api/routes/user')(app)
 }
