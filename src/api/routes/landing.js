efabacefd6287397b16ffe6fcdde0216644c1dbe
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-08-25 09:32:36 +0800

    Added landing routes

--- src/api/routes/landing.js
+++ src/api/routes/landing.js
@@ -1,18 +1,27 @@
-module.exports = (app) => {
+const authentication = require('../middlewares/authentication')
 
-    // index page
-    app.get('', (req, res) => {
-        res.render('index', {
-            title: 'DMS',
-            author: 'Almer F. Tampus'
+module.exports = async (app) => {
+    // Index
+    app.get('', async (req, res) => {
+        res.render('components/login', {
+            layout: 'layouts/index',
+            title: 'Login'
         })
     })
 
-    // signup page
-    app.get('/signup', (req, res) => {
-        res.render('signup', {
-            title: 'Signup',
-            author: 'Almer F. Tampus'
+    // Signup
+    app.get('/signup', async (req, res) => {
+        res.render('components/signup', {
+            layout: 'layouts/index',
+            title: 'Signup'
+        })
+    })
+
+    // Home
+    app.get('/home', authentication, async (req, res) => {
+        res.render('components/landing/home', {
+            layout: 'layouts/index',
+            title: 'Home'
         })
     })
 }
