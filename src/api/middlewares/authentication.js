31110d7b97a04d9bb3634305fee783013d5a5918
Almer Tampus <almerflorestampus@gmail.com>
2020-09-02 13:39:14 +0800

    Integrated sessions into authentication middleware

--- src/api/middlewares/authentication.js
+++ src/api/middlewares/authentication.js
@@ -1,20 +1,29 @@
 const jwt = require('jsonwebtoken')
-require('cookie-parser')
 const User = require('../../models/user')
 
 const authentication = async (req, res, next) => {
     try {
-        const token = req.cookies.accessToken
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        if (req.cookies.sessionId === req.session.id) {
+            const token = req.session.token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
-        if (!user) {
-            throw new Error()
+            if (!user) {
+                console.log('Authentication required!')
+                res.status(401)
+                    .redirect('/')
+            }
+
+            next()
+        } else {
+            console.log('Authentication required!')
+            res.status(401)
+                .redirect('/')
         }
-
-        next()
     } catch (e) {
-        res.redirect('/')
+        console.log('Authentication required!')
+        res.status(401)
+            .redirect('/')
     }
 }
 
