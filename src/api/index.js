321a7579ce551b13158174c2831f702b7b267fd1
Almer Tampus <almerflorestampus@gmail.com>
2020-09-02 14:59:20 +0800

    Added property routes

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 const userRouter = require('./routes/user')
+const propertyRouter = require('./routes/property')
 
 module.exports = async (app) => {
     await require('./routes/landing')(app)
     app.use(userRouter)
+    app.use(propertyRouter)
 }
--- src/api/routes/property.js
+++ src/api/routes/property.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { propertyNew } = require('../../services/property')
+const { propertyNew, propertyList } = require('../../services/property')
 const authentication = require('../middlewares/authentication')
 
 const router = new express.Router()
@@ -11,6 +11,23 @@ router.post('/user/property/add', authentication, async (req, res) => {
 
         res.status(201)
             .redirect('/property')
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.get('/property', authentication, async (req, res) => {
+    try {
+        const { properties } = await propertyList(req.session.userId)
+        console.log(properties)
+        res.status(200)
+            .render('components/landing/property', {
+                layout: 'layouts/main',
+                title: 'Property',
+                username: req.session.userName,
+                email: req.session.userEmail,
+                properties: properties
+            })
     } catch (e) {
         res.status(400).send()
     }
