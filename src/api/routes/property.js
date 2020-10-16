caed8d6d83a67d83587574b820d3feb00bda776d
Almer F. Tampus <almerflorestampus@gmail.com>
2020-10-17 06:56:30 +0800

    Added view property

--- src/api/routes/property.js
+++ src/api/routes/property.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const multer = require('multer')
 const { v4: uuidv4 } = require('uuid')
-const { propertyNew, propertyList } = require('../../services/property')
+const { propertyNew, propertyList, propertyView} = require('../../services/property')
 const authentication = require('../middlewares/authentication')
 
 const router = new express.Router()
@@ -18,7 +18,7 @@ const uploadPropertyAvatar = multer({ storage: storage })
 // Create new property
 router.post('/user/property/add', authentication, uploadPropertyAvatar.any(), async (req, res) => {
     try {
-        await propertyNew(req.body, req.files, req.session.user)
+        await propertyNew(req.body, req.session.user)
 
         res.status(201)
             .redirect('/property')
@@ -28,16 +28,37 @@ router.post('/user/property/add', authentication, uploadPropertyAvatar.any(), as
     }
 })
 
+// List properties
 router.get('/property', authentication, async (req, res) => {
     try {
         const { properties } = await propertyList(req.session.user)
 
         res.status(200)
-            .render('pages/landing/property', {
+            .render('pages/landing/property/list', {
                 layout: 'index',
                 title: 'Property',
                 user: req.session.user,
                 properties: properties
+            })
+    } catch (e) {
+        res.status(400)
+            .send(e)
+    }
+})
+
+// View property
+router.get('/property/:id', authentication, async (req, res) => {
+    try {
+        const { properties } = await propertyList(req.session.user)
+        const { property } = await propertyView(req.params.id, req.session.user)
+
+        res.status(200)
+            .render('pages/landing/property/view', {
+                layout: 'index',
+                title: property.name,
+                user: req.session.user,
+                properties: properties,
+                property: property
             })
     } catch (e) {
         res.status(400)
--- src/services/property.js
+++ src/services/property.js
@@ -1,28 +1,35 @@
 const Property = require('../models/property')
 
 // Create property
-const propertyNew = async (propertyObject, fileObject, userObject) => {
-    const avatar = fileObject[0].filename
+const propertyNew = async (propertyObject, userObject) => {
     const property = new Property({
         name: propertyObject.name,
         location: propertyObject.location,
         about: propertyObject.about,
         room: propertyObject.room,
         available: propertyObject.room,
-        avatar,
         author: userObject._id
     })
 
     await property.save()
 }
 
+// List properties
 const propertyList = async (userObject) => {
     const properties = await Property.find({ author: userObject._id })
 
     return { properties }
 }
 
+// View property
+const propertyView = async (propertyId, userObject) => {
+    const property = await Property.findOne({ _id: propertyId, author: userObject._id })
+
+    return { property }
+}
+
 module.exports = {
     propertyNew,
-    propertyList
+    propertyList,
+    propertyView
 } 
