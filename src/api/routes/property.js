e66904810a46bd3fa2f6cbd7d0bd5dd772b18d36
Almer F. Tampus <almerflorestampus@gmail.com>
2020-11-06 13:18:34 +0800

    Added rooms

--- src/api/routes/property.js
+++ src/api/routes/property.js
@@ -2,27 +2,28 @@ const path = require('path')
 const express = require('express')
 const multer = require('multer')
 const { v4: uuidv4 } = require('uuid')
-const { propertyNew, propertyList, propertyView} = require('../../services/property')
-const { catalogList } = require('../../services/catalog')
 const authentication = require('../middlewares/authentication')
+const { propertyNew, propertyList, propertyView } = require('../../services/property')
+const { catalogList } = require('../../services/catalog')
+const { roomList } = require('../../services/room')
 
 const router = new express.Router()
 
 const storage = multer.diskStorage({
     destination: path.join(__dirname, '../../../public/uploads/property'),
-    filename: function (req, file, cb) {
+    filename: function(req, file, cb) {
         cb(null, uuidv4())
     }
 })
 const uploadPropertyAvatar = multer({ storage: storage })
 
 // Create new property
-router.post('/user/property/add', authentication, uploadPropertyAvatar.any(), async (req, res) => {
+router.post('/user/property/add', authentication, uploadPropertyAvatar.any(), async(req, res) => {
     try {
         await propertyNew(req.body, req.session.user)
 
         res.status(201)
-            .redirect('/')
+            .redirect('/management/property')
     } catch (e) {
         res.status(400)
             .send()
@@ -30,20 +31,23 @@ router.post('/user/property/add', authentication, uploadPropertyAvatar.any(), as
 })
 
 // View property
-router.get('/property/:id', authentication, async (req, res) => {
+router.get('/property/:id', authentication, async(req, res) => {
     try {
         const { properties } = await propertyList(req.session.user)
         const { property } = await propertyView(req.params.id, req.session.user)
         const { catalogs } = await catalogList(req.session.user)
+        const { rooms } = await roomList(req.params.id)
 
         res.status(200)
             .render('pages/landing/property/view', {
                 layout: 'index',
                 title: property.name,
+                header: 'Property',
                 user: req.session.user,
                 properties,
                 property,
-                catalogs
+                catalogs,
+                rooms
             })
     } catch (e) {
         res.status(400)
