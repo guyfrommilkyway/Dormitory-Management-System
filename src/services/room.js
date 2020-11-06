8b37528b4f2fad8b625d752046d76943df2059cf
Almer F. Tampus <almerflorestampus@gmail.com>
2020-11-06 13:17:49 +0800

    Added room list

--- src/services/room.js
+++ src/services/room.js
@@ -1,12 +1,32 @@
 const Room = require('../models/room')
+const { v4: uuidv4 } = require('uuid')
 
 // Create room
-const roomNew = async (roomObject) => {
-    const room = new Room(roomObject)
+const roomNew = async(roomObject) => {
+    let i = 0
+ 
+    for (i = 0; i < roomObject.number; i++) {
+        const room = new Room({
+            catalog: roomObject.catalog,
+            name: 'Room-' + uuidv4(),
+            floor: roomObject.floor,
+            property: roomObject.property
+        })
 
-    await room.save()
+        await room.save()
+    }
+}
+
+// List rooms
+const roomList = async(propertyId) => {
+    const rooms = await Room.find({ property: propertyId })
+        .populate('catalog')
+        .exec()
+
+    return { rooms }
 }
 
 module.exports = {
-    roomNew
-} 
+    roomNew,
+    roomList
+}
