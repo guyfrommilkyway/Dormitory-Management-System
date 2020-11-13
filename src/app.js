8f3c2696f758a4c04fd183ef3601ed743ed6f2dd
Almer Tampus <almerflorestampus@gmail.com>
2020-11-14 03:22:47 +0800

    Restructured application setup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
-const express = require('express')
-
 async function startServer() {
-    // Initialize express application
-    const app = express()
-
     // Database connection
     await require('../src/loaders/mongoose')
 
     // Express
-    await require('../src/loaders/express')(app, express)
+    await require('../src/loaders/express')
 }
 
 startServer()
--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,37 +1,23 @@
 const path = require('path')
+const express = require('express')
 const hbs = require('hbs')
 const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const { v4: uuidv4 } = require('uuid')
 
-module.exports = async (app, express) => {
-    // Express
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
-    app.use(cookieParser())
-    app.use(session({
-        genid: function (req) {
-            return uuidv4()
-        },
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            secure: false,
-        }
-    }))
-    app.use(express.static(path.join(__dirname, '../../public')))
-    app.use('/mdbootstrap', express.static(path.join(__dirname, '../../node_modules/mdbootstrap')))
-    app.listen(process.env.PORT, () => {
-        console.log('The server is up on port ' + process.env.PORT + '.')
-    })
+const app = express()
 
-    // Handlebars
-    app.set('view engine', 'hbs')
-    app.engine('hbs', hbs.__express)
-    app.set('views', path.join(__dirname, '../../templates/'))
-    hbs.registerPartials(path.join(__dirname, '../../templates/components'))
+app.engine('hbs', hbs.__express)
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, '../../templates/'))
+hbs.registerPartials(path.join(__dirname, '../../templates/components'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, '../../public')))
+app.use('/mdbootstrap', express.static(path.join(__dirname, '../../node_modules/mdbootstrap')))
 
-    // Routes
-    await require('../api/index')(app)
-}
+app.listen(process.env.PORT, () => {
+    console.log('Application is up and running on port ' + process.env.PORT + '.')
+})
+
+// Routes
+require('../api/index')(app)
