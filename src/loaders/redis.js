204aecc3a4064aaecbe466695a37fda773ed634e
Almer Tampus <almerflorestampus@gmail.com>
2020-11-18 03:01:09 +0800

    Setup redis configuration

--- src/loaders/redis.js
+++ src/loaders/redis.js
@@ -1,7 +1,11 @@
 const { promisify } = require('util')
 const redis = require("redis")
 
-const client = redis.createClient({ port: process.env.REDIS_PORT })
+const client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+})
 
 client.on('error', (err) => {
     console.log(err)
