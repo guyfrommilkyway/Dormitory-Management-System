ea1ea14cc4793e0223b472ab93a650e38dc1563f
Almer Tampus <almerflorestampus@gmail.com>
2020-11-20 04:55:33 +0800

    Added default-profile-photo

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,12 @@
-// Upload profile avatar
-function upload(input) {
-    if (input.files && input.files[0]) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            $('#uploaded-image')
-                .attr('style', 'width: 6rem; height: 6rem; background-image: url(' + e.target.result + '); border-radius: 3rem;');
-            $('#uploaded-message')
-                .attr('style', 'display: none;');
-            $('.upload')
-                .attr('style', 'border: none;');
-        };
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-
-// Upload property avatar
+// Upload profile-photo
 function update(input) {
     if (input.files && input.files[0]) {
         const reader = new FileReader();
         reader.onload = function (e) {
-            $('#old_image')
+            $('#current_profile_photo')
                 .attr('style', 'display: none;');
-            $('#updated_image')
-            .attr('style', 'display: block; background-image: url(' + e.target.result + ');');
+            $('#uploaded_profile_photo')
+                .attr('style', 'display: flex; background-image: url(' + e.target.result + ');');
         };
         reader.readAsDataURL(input.files[0]);
     }
