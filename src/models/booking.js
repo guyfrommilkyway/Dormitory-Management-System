64ad79697a2b8f2b095d90956256896a3304c1d0
Almer Tampus <almerflorestampus@gmail.com>
2020-11-20 09:42:27 +0800

    Added booking model

---
+++ src/models/booking.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+// Booking model
+const bookingSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    last_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    mobile: {
+        type: String,
+        min: 11,
+        max: 11,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Please provide a valid email address')
+            }
+        }
+    },
+    property: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Property'
+    },
+}, {
+    timestamps: true
+})
+
+
+const Booking = mongoose.model('Booking', bookingSchema)
+
+module.exports = Booking
