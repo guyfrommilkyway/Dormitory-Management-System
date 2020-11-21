f2251e1b40265acad729cf67e7d3c1464d66ad7b
Almer Tampus <almerflorestampus@gmail.com>
2020-11-21 10:59:21 +0800

    Updated booking routes

--- src/api/routes/admin/booking.js
+++ src/api/routes/admin/booking.js
@@ -4,10 +4,10 @@ const { bookingNew } = require('../../../services/booking')
 const router = new express.Router()
 
 // Create new booking
-router.post('/api/booking/add', async (req, res) => {
+router.post('/api/booking/:id', async (req, res) => {
     try {
         // Create new booking
-        await bookingNew(req.body)
+        await bookingNew(req.body, req.params.id)
 
         res.status(201)
             .redirect('/booking')
