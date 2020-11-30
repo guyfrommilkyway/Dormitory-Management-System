459343c03ae8bb0c413b79f56b9c91e6d08b80ec
Almer Tampus <almerflorestampus@gmail.com>
2020-11-30 19:41:23 +0800

    Updated authentication error handling

--- src/api/routes/admin/landing.js
+++ src/api/routes/admin/landing.js
@@ -25,16 +25,21 @@ module.exports = async (app, handlebars) => {
 
     // Sign up
     app.get('/signup', async (req, res) => {
-        // Compile template
-        const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/signup.hbs'), 'utf8'));
+        if (req.user && req.token) {
+            res.status(401)
+                .redirect('/')
+        } else {
+            // Compile template
+            const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/signup.hbs'), 'utf8'));
 
-        // Render template
-        const output = template({
-            title: 'Sign up'
-        });
+            // Render template
+            const output = template({
+                title: 'Sign up'
+            });
 
-        res.status(200)
-            .send(output)
+            res.status(200)
+                .send(output)
+        }
     })
 
     // Log in
@@ -73,150 +78,175 @@ module.exports = async (app, handlebars) => {
 
     // Property
     app.get('/property/:id', authentication, async (req, res) => {
-        // Query properties
-        const { properties } = await propertyList(req.user._id)
+        if (req.user && req.token) {
+            // Query properties
+            const { properties } = await propertyList(req.user._id)
 
-        // Get specific property
-        const { property } = await propertyGet(req.params.id)
+            // Get specific property
+            const { property } = await propertyGet(req.params.id)
 
-        // Query catalogs
-        const { catalogs } = await catalogList(req.user._id)
+            // Query catalogs
+            const { catalogs } = await catalogList(req.user._id)
 
-        // Query rooms in specific property
-        const { rooms } = await roomList(req.params.id)
+            // Query rooms in specific property
+            const { rooms } = await roomList(req.params.id)
 
-        // Vacant rooms
-        let vacant_rooms = []
-        for (let i = 0; i < rooms.length; i++) {
-            if (rooms[i].tenant === null) {
-                vacant_rooms.push(rooms[i]);
+            // Vacant rooms
+            let vacant_rooms = []
+            for (let i = 0; i < rooms.length; i++) {
+                if (rooms[i].tenant === null) {
+                    vacant_rooms.push(rooms[i]);
+                }
             }
-        }
 
-        // Occupied rooms
-        let occupied_rooms = []
-        for (let i = 0; i < rooms.length; i++) {
-            if (rooms[i].tenant != null) {
-                occupied_rooms.push(rooms[i]);
+            // Occupied rooms
+            let occupied_rooms = []
+            for (let i = 0; i < rooms.length; i++) {
+                if (rooms[i].tenant != null) {
+                    occupied_rooms.push(rooms[i]);
+                }
             }
+
+            // Query tenants in specific property
+            const { tenants } = await tenantList(req.params.id)
+
+            // Query bookings in specific property
+            const { bookings } = await bookingList(req.params.id)
+
+            // Compile template
+            const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/property.hbs'), 'utf8'));
+
+            // Render template
+            const output = template({
+                title: property.name,
+                header: 'Property / ' + property.name,
+                user: req.user,
+                properties,
+                catalogs,
+                property,
+                rooms,
+                vacant_rooms: vacant_rooms.length,
+                occupied_rooms: occupied_rooms.length,
+                total_rooms: rooms.length,
+                tenants,
+                total_tenants: tenants.length,
+                bookings,
+                total_bookings: bookings.length
+            });
+
+            res.status(200)
+                .send(output)
+        } else {
+            res.status(401)
+                .redirect('/')
         }
-
-        // Query tenants in specific property
-        const { tenants } = await tenantList(req.params.id)
-
-        // Query bookings in specific property
-        const { bookings } = await bookingList(req.params.id)
-
-        // Compile template
-        const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/property.hbs'), 'utf8'));
-
-        // Render template
-        const output = template({
-            title: property.name,
-            header: 'Property / ' + property.name,
-            user: req.user,
-            properties,
-            catalogs,
-            property,
-            rooms,
-            vacant_rooms: vacant_rooms.length,
-            occupied_rooms: occupied_rooms.length,
-            total_rooms: rooms.length,
-            tenants,
-            total_tenants: tenants.length,
-            bookings,
-            total_bookings: bookings.length
-        });
-
-        res.status(200)
-            .send(output)
     })
 
     // Management
 
     // Property
     app.get('/management/property', authentication, async (req, res) => {
-        // Query properties
-        const { properties } = await propertyList(req.user._id)
+        if (req.user && req.token) {
+            // Query properties
+            const { properties } = await propertyList(req.user._id)
 
-        // Compile template
-        const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/management/property.hbs'), 'utf8'));
+            // Compile template
+            const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/management/property.hbs'), 'utf8'));
 
-        // Render template
-        const output = template({
-            title: 'Property Management',
-            header: 'Management / Property',
-            user: req.user,
-            properties
-        });
+            // Render template
+            const output = template({
+                title: 'Property Management',
+                header: 'Management / Property',
+                user: req.user,
+                properties
+            });
 
-        res.status(200)
-            .send(output)
+            res.status(200)
+                .send(output)
+        } else {
+            res.status(401)
+                .redirect('/')
+        }
     })
 
     // Catalog
     app.get('/management/catalog', authentication, async (req, res) => {
-        // Query properties
-        const { properties } = await propertyList(req.user._id)
+        if (req.user && req.token) {
+            // Query properties
+            const { properties } = await propertyList(req.user._id)
 
-        // Query catalogs
-        const { catalogs } = await catalogList(req.user._id)
+            // Query catalogs
+            const { catalogs } = await catalogList(req.user._id)
 
-        // Compile template
-        const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/management/catalog.hbs'), 'utf8'));
+            // Compile template
+            const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/management/catalog.hbs'), 'utf8'));
 
-        // Render template
-        const output = template({
-            title: 'Catalog Management',
-            header: 'Management / Catalog',
-            user: req.user,
-            properties,
-            catalogs
-        });
+            // Render template
+            const output = template({
+                title: 'Catalog Management',
+                header: 'Management / Catalog',
+                user: req.user,
+                properties,
+                catalogs
+            });
 
-        res.status(200)
-            .send(output)
+            res.status(200)
+                .send(output)
+        } else {
+            res.status(401)
+                .redirect('/')
+        }
     })
 
     // Account
 
     // Edit profile
     app.get('/account/profile', authentication, async (req, res) => {
-        // Query properties
-        const { properties } = await propertyList(req.user._id)
+        if (req.user && req.token) {
+            // Query properties
+            const { properties } = await propertyList(req.user._id)
 
-        // Compile template
-        const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/account/profile.hbs'), 'utf8'));
+            // Compile template
+            const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/account/profile.hbs'), 'utf8'));
 
-        // Render template
-        const output = template({
-            title: 'Profile',
-            header: 'Profile',
-            user: req.user,
-            properties
-        });
+            // Render template
+            const output = template({
+                title: 'Profile',
+                header: 'Profile',
+                user: req.user,
+                properties
+            });
 
-        res.status(200)
-            .send(output)
+            res.status(200)
+                .send(output)
+        } else {
+            res.status(401)
+                .redirect('/')
+        }
     })
 
     // Change password
     app.get('/account/password', authentication, async (req, res) => {
-        // Query properties
-        const { properties } = await propertyList(req.user._id)
+        if (req.user && req.token) {
+            // Query properties
+            const { properties } = await propertyList(req.user._id)
 
-        // Compile template
-        const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/account/password.hbs'), 'utf8'));
+            // Compile template
+            const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/admin/account/password.hbs'), 'utf8'));
 
-        // Render template
-        const output = template({
-            title: 'Security',
-            header: 'Security',
-            user: req.user,
-            properties
-        });
+            // Render template
+            const output = template({
+                title: 'Security',
+                header: 'Security',
+                user: req.user,
+                properties
+            });
 
-        res.status(200)
-            .send(output)
+            res.status(200)
+                .send(output)
+        } else {
+            res.status(401)
+                .redirect('/')
+        }
     })
 }
