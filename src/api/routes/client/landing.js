20ef77c9463db4225ed4c164c0bb569b8389cafb
guyfrommilkyway <almerflorestampus@gmail.com>
2021-01-19 10:16:59 +0800

    Change 'property.owner' to 'property._id'

--- src/api/routes/client/landing.js
+++ src/api/routes/client/landing.js
@@ -6,12 +6,12 @@ const { catalogList } = require('../../../services/catalog')
 module.exports = async (app, handlebars) => {
 
     // Booking
-    app.get('/booking/:id', async (req, res) => {
+    app.get("/booking/:id", async (req, res) => {
         // Query property
         const { property } = await propertyView(req.params.id)
 
         // Query catalogs
-        const { catalogs } = await catalogList(property.owner)
+        const { catalogs } = await catalogList(property._id)
 
         // Compile template
         const template = await handlebars.compile(fs.readFileSync(path.join(__dirname, '../../../../views/pages/client/booking/booking.hbs'), 'utf8'));
