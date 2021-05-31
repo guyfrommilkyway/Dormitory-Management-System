74ca275b125ab67f08e976a67ad77ebc15c2d524
guyfrommilkyway <almerflorestampus@gmail.com>
2021-06-01 04:34:31 +0800

    Add booking decline route

--- src/api/routes/admin/booking.js
+++ src/api/routes/admin/booking.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { bookingNew } = require('../../../services/booking')
+const authentication = require('../../middlewares/authentication')
+const { bookingNew, bookingDecline } = require('../../../services/booking')
 
 const router = new express.Router()
 
 // Create new booking
-router.post('/api/booking/:id', async (req, res) => {
+router.post('/api/booking/list/:id', async (req, res) => {
     try {
         // Create new booking
         await bookingNew(req.body, req.params.id)
@@ -20,6 +21,26 @@ router.post('/api/booking/:id', async (req, res) => {
 
         res.status(400)
             .redirect(`/booking/${id}`)
+    }
+})
+
+// Decline booking
+router.post('/api/booking/decline', authentication, async (req, res) => {
+    try {
+        // Delete booking
+        await bookingDecline(req.body)
+
+        //Assign id
+        const _id = req.body.property
+
+        res.status(200)
+            .redirect(`/property/${_id}` + '#.js-booking-panel')
+    } catch (e) {
+        //Assign id
+        const _id = req.body.property
+
+        res.status(400)
+            .redirect(`/property/${_id}` + '#.js-booking-panel')
     }
 })
 
