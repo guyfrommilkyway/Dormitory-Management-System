815ebda95a31072d5cd13668ed42b5bdf36dce4c
Almer Tampus <almerflorestampus@gmail.com>
2022-03-08 19:27:31 +0800

    REMOVE unnecessary code

--- src/api/middlewares/authentication.js
+++ src/api/middlewares/authentication.js
@@ -1,44 +1,42 @@
-const jwt = require('jsonwebtoken')
-const { client, getAsync } = require('../../loaders/redis')
-const User = require('../../models/user')
+const jwt = require('jsonwebtoken');
+const { client, getAsync } = require('../../loaders/redis');
+const User = require('../../models/user');
 
 const authentication = async (req, res, next) => {
     try {
         // Get sessionId in cookie
-        const sessionId = req.signedCookies.sessionId
+        const sessionId = req.signedCookies.sessionId;
 
         // Check if sessionId is associated with a record in the redis database
         if (!sessionId || !sessionId === '') {
-            throw new Error()
+            throw new Error();
         }
 
         // Check if sessionId has a token
-        async () => { client.hget('user', sessionId) }
-        const token = await getAsync('user', sessionId)
+        async () => { client.hget('user', sessionId); };
+        const token = await getAsync('user', sessionId);
 
         if (!token || token === '') {
-            throw new Error()
+            throw new Error();
         }
 
         // Validate token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
         if (!user) {
-            throw new Error()
+            throw new Error();
         }
 
-        let userStringified = JSON.stringify(user.toJSON())
+        req.user = JSON.parse(user);
+        req.token = token;
 
-        req.user = JSON.parse(userStringified)
-        req.token = token
-
-        next()
+        next();
     } catch (e) {
-        console.log('Error: Authentication is required.')
+        console.log('Error: Authentication is required.');
 
-        next()
+        next();
     }
-}
+};
 
-module.exports = authentication
+module.exports = authentication;
